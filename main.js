059949ee9765a32b34a477e63b15d4b9dd486eb4
Mili <milagrossalonsoo@gmail.com>
2022-08-03 21:09:56 -0300

    shopping-cart functions

--- main.js
+++ main.js
@@ -1,8 +1,27 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const hamMenuIcon = document.querySelector('.menu');
+const menuCartIcon = document.querySelector('.navbar-shopping-cart');
+const mobileMenu = document.querySelector('.mobile-menu');
+const aside = document.querySelector('.product-detail');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
+hamMenuIcon.addEventListener('click', toggleMobileMenu);
+menuCartIcon.addEventListener('click', toggleMenuCartIcon);
 
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle('inactive');
+}
+
+function toggleMobileMenu() {
+    mobileMenu.classList.toggle('inactive');
+}
+
+function toggleMenuCartIcon() {
+    const isMobileMenuOpen = mobileMenu.classList.contains('inactive');
+    const isAsideOpen = aside.classList.contains('inactive');
+    
+    aside.classList.toggle('inactive');
+
+    if (mobileMenu.classList.contains('inactive'))
 }
